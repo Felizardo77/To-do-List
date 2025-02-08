b5c01c86a4252d57808faeb0795b583b3dc89e62
Felizardo <felizardopaulo40@gmail.com>
2025-02-08 11:13:24 +0200

    feature: Adiciona Tarefas e as salva no Local Storage

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 const hoje_btn = document.querySelector(".today_btn");
 hoje_btn.addEventListener("click", () => {
   const display = document.querySelector(".display_content");
+  display.innerHTML = ""
 
   const hoje_span = document.createElement("span");
   hoje_span.innerText = "Hoje";
@@ -19,14 +21,17 @@ hoje_btn.addEventListener("click", () => {
   });
 
   task_container.append(add_task);
-
-  //Mostrar todas as tarefas de Hoje
-
   //Adicionar os elementos ao parent
   display.append(hoje_span, task_container);
+
+  //Mostrar todas as tarefas de Hoje
+  const tarefas = loadTasks()
+  tarefas.forEach(item =>{
+    addTask(item)
+  })
 });
 
-function addTask() {
+function addTask(taskData=null) {
   //Adiciona uma nova Tarefa
   const tasks = document.querySelector('.task_container')
   const task = document.createElement("div");
@@ -44,9 +49,38 @@ function addTask() {
   taskText.addEventListener('keypress',(ev)=>{
     if(ev.key === 'Enter'){
         taskText.blur()
+        saveTask({
+            concluido:taskCheck.checked,
+            tarefa:taskText.value
+        })
     }
   })
 
+  //Adicionar dados a task se for o caso
+  if(taskData !== null){
+    taskCheck.checked = taskData.concluido
+    taskText.value = taskData.tarefa
+  }
+
   task.append(taskCheck, taskText);
   tasks.append(task);
+}
+
+function loadTasks(){
+    let tarefasString = window.localStorage.getItem('tarefas')
+    let tarefas = JSON.parse(tarefasString)
+    return tarefas
+}
+
+function saveTasks(tarefas){
+    window.localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
+function saveTask(tarefa){
+    if(loadTasks() === null){
+        saveTasks([])
+    }
+    let tarefas = loadTasks()
+    tarefas.push(tarefa)
+    saveTasks(tarefas)   
 }
